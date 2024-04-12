324cee6447e4c6113992f2d0388568b335e45666
Diego Arcos Caceres <105613359+DIEGOACL07@users.noreply.github.com>
2024-04-11 20:14:00 -0500

    Add files via upload

---
+++ data/db.ts
@@ -0,0 +1,64 @@
+import type { MenuItem } from '../types'
+
+export const menuItems : MenuItem[] = [
+    {
+      "id": 1,
+      "name": "Ceviche",
+      "price": 30
+    },
+    {
+      "id": 2,
+      "name": "Pollo a la Brasa",
+      "price": 50
+    },
+    {
+      "id": 3,
+      "name": "Causa Rellena",
+      "price": 30
+    },
+    {
+      "id": 4,
+      "name": "Papa a la Huancaína",
+      "price": 30
+    },
+    {
+      "id": 5,
+      "name": "Ají de Gallina",
+      "price": 25
+    },
+    {
+      "id": 6,
+      "name": "Anticuchos",
+      "price": 20
+    },
+    {
+      "id": 7,
+      "name": "Lomo Saltado",
+      "price": 30
+    },
+    {
+      "id": 8,
+      "name": "Arroz con Pollo",
+      "price": 25
+    },
+    {
+      "id": 9,
+      "name": "Pachamanca",
+      "price": 40
+    },
+    {
+      "id": 10,
+      "name": "Juane peruano",
+      "price": 30
+    },
+    {
+      "id": 11,
+      "name": "Papa Rellena",
+      "price": 25
+    },
+    {
+      "id": 12,
+      "name": "Arroz Chaufa Peruano",
+      "price": 20
+    }
+]
